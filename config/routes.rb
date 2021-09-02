0bcf470069f211c5425d0f1ee7af639604d59c4a
naoko kasahara <kassa0918@gmail.com>
2021-09-02 13:10:02 +0900

    devise_userカラム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {sessions: 'admins/sessions'}
-  devise_for :users
+  devise_for :users , controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
   root 'tops#show'
   resource :top, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
