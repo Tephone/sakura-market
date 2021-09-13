536e38ba315ca55700ba6b6b487da1e3fbc69b5e
naoko kasahara <kassa0918@gmail.com>
2021-09-13 18:59:45 +0900

    sellerカラム追加、作成編集削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :users, only: %i[index show edit update destroy]
     resources :products
     resources :coupons
+    resources :sellers
   end
   namespace :users do
     resource :mypage, only: %i[show]
